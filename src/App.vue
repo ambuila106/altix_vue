cc0982d9812a0d0fae37eb9fa116679b15a16ac6
Brandon Ambuila <Ambuila@Edwins-iMac.local>
2019-08-30 17:43:58 -0500

    Subo la estructura del proyecto al repositorio de GitHub

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" v-bind:src="message">
+    prueba
+    <HelloWorld msg="Task "/>
+    <ul>
+      <li v-for="task in tasks">
+        {{task.task_todo}}
+      </li>
+    </ul>
+    <a ></a>{{message}}
   </div>
+  
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      tasks: [],
+      message: '/img/logo.82b9c7a5.png'
+    }
+  },
+  mounted(){
+    axios.get('http://localhost:8000/api/task/')
+    .then((response) => {
+      // handle success
+      console.log(response.data);
+      this.tasks = response.data;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
   }
 }
+ 
 </script>
 
 <style>
